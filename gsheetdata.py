7a66484057df029b4a3954b28b65db63605e5e2b
GoldenMM <ryanjanderton@yahoo.com.au>
2021-03-13 03:21:38 +0800

    commiting the first few files

---
+++ gsheetdata.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 19:22:09 2021
+@author: supremegoldenmm
+
+Library of functions for reading and writing data to a Google Sheet
+"""
+import pandas as pd
+import pygsheets
+
+#authorization
+gc = pygsheets.authorize(service_file = 
+   '/home/supremegoldenmm/Python Coding/TAB Model/service_key.json')
+
+
+
+
+
+
+
+
+
